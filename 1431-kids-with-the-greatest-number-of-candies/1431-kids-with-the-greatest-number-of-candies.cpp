1108c8763442c9a19f09e9fd80348c6ee421198d
Akash Shinde <96474874+Mr-AkashShinde@users.noreply.github.com>
2022-11-29 22:26:53 +0530

    Time: 6 ms (46.26%), Space: 9 MB (13.26%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> vb ;
         
-        vector<bool> out;
-        int max = *max_element(candies.begin(), candies.end());
-        
-        for(int i=0; i < candies.size(); i++){
-            if(candies[i]+extraCandies >= max)
-                out.push_back(true);
-            else
-                out.push_back(false);
+        for(int i=0; i<candies.size(); i++){
+            if(candies[i]+extraCandies >= *max_element(candies.begin(), candies.end())){
+                vb.push_back(true);
+            }
+            if(candies[i]+extraCandies < *max_element(candies.begin(), candies.end())){
+                vb.push_back(false);
+            }
+            
         }
         
-        return out;
+        return vb;
     }
 };
