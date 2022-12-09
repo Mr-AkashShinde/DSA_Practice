1b3d1250e53feb602e1f318fe7687c5490d0fa9a
Akash Shinde <96474874+Mr-AkashShinde@users.noreply.github.com>
2022-12-09 23:47:55 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (59.86%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     char repeatedCharacter(string s) {
-        string temp="";
+        
         for(int i=0; i<s.length(); i++){
             
-            for(int j=0; j<temp.length(); j++){
-                if(s[i]==temp[j]){
+            for(int j=0; j<i; j++){
+                if(s[i]==s[j]){
                     return s[i];
                 }
             }
-            temp+=s[i];
+            
         }
         return s[0];
     }
