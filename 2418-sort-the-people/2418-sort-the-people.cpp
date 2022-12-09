dd9f806d32de5c9271d19ece54d00810e46a6f71
Akash Shinde <96474874+Mr-AkashShinde@users.noreply.github.com>
2022-12-09 22:59:32 +0530

    Time: 578 ms (5.02%), Space: 20.2 MB (86.49%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        int temp=0;
+        string temp1;
         for(int i=0; i<heights.size(); i++){
           for(int j=0;j<heights.size();j++){
               if(heights[i]>heights[j]){
-                 swap(heights[i],heights[j]); 
-                 swap(names[i],names[j]);
+                 // swap(heights[i],heights[j]); 
+                 // swap(names[i],names[j]);
+                  temp=heights[i];
+                  heights[i]=heights[j];
+                  heights[j]=temp;
+                  
+                  temp1=names[i];
+                  names[i]=names[j];
+                  names[j]=temp1;
               }
           }
         }
+        cout<<heights[0];
         return names;
     }
 };
