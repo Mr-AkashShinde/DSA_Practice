da4ab66b5453579ac7e7abead409af0fd80d1526
Akash Shinde <96474874+Mr-AkashShinde@users.noreply.github.com>
2023-04-11 22:24:11 +0530

    Time: 149 ms (46.27%), Space: 71.7 MB (18.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i]==nums[i+1]){
+        // sort(nums.begin(), nums.end());
+        // for(int i=0; i<nums.size()-1; i++){
+        //     if(nums[i]==nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        unordered_map<int, int> m;
+        for(auto x:nums){
+            m[x]++;
+        }
+        for(auto x:m){
+            if(x.second>1){
                 return true;
             }
         }
