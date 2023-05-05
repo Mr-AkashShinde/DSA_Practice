ab2120528f1fe17a7aadb98af7c0102a7f4e3d4b
Akash Shinde <96474874+Mr-AkashShinde@users.noreply.github.com>
2023-05-05 19:53:16 +0530

    Time: 4 ms (72.70%), Space: 9.2 MB (52.53%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+        string ans="";
+        for(int i=0; i<strs[0].size(); i++){
+            char ch = strs[0][i];
+            
+            bool match = true;
+            
+            for(int j=1; j<strs.size(); j++){
+                if(ch != strs[j][i]){
+                    match = false;
+                    break;
+                }
+            }
+            if(match == false)
+                break;
+            else
+                ans.push_back(ch);
+        }
+        return ans;
+    }
+};
