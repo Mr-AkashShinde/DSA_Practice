b11a66b13193d80ae9240708a4ce0f27c164e67a
Akash Shinde <96474874+Mr-AkashShinde@users.noreply.github.com>
2023-07-05 12:12:22 +0530

    Time: 5 ms (22.08%), Space: 7.9 MB (64.60%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -3,40 +3,40 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> ans;
         
-        for(int i=1; i<=n; i++){
-            bool divisibleBy3 = i%3==0;
-            bool divisibleBy5 = i%5==0;
+//         for(int i=1; i<=n; i++){
+//             bool divisibleBy3 = i%3==0;
+//             bool divisibleBy5 = i%5==0;
             
-            if(divisibleBy3 && divisibleBy5){
-                ans.push_back("FizzBuzz");
-            }
-            else if(divisibleBy3){
-                ans.push_back("Fizz");
-            }
-            else if(divisibleBy5){
-                ans.push_back("Buzz");
-            }
-            else{
-                ans.push_back(to_string(i));
-            }
-        }
-//          for(int i=1; i<=n; i++){
-//             // bool divisibleBy3 = i%3==0;
-//             // bool divisibleBy5 = i%5==0;
-            
-//             if(i%3==0 && i%5==0){
-//                 ans.push_back("Fizzbuzz");
+//             if(divisibleBy3 && divisibleBy5){
+//                 ans.push_back("FizzBuzz");
 //             }
-//             else if(i%3==0){
+//             else if(divisibleBy3){
 //                 ans.push_back("Fizz");
 //             }
-//             else if(i%5==0){
+//             else if(divisibleBy5){
 //                 ans.push_back("Buzz");
 //             }
 //             else{
 //                 ans.push_back(to_string(i));
 //             }
 //         }
+         for(int i=1; i<=n; i++){
+            // bool divisibleBy3 = i%3==0;
+            // bool divisibleBy5 = i%5==0;
+            
+            if(i%3==0 && i%5==0){
+                ans.push_back("FizzBuzz");
+            }
+            else if(i%3==0){
+                ans.push_back("Fizz");
+            }
+            else if(i%5==0){
+                ans.push_back("Buzz");
+            }
+            else{
+                ans.push_back(to_string(i));
+            }
+        }
         return ans;
     }
 };
